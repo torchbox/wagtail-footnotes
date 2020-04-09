cc017ae1734b745840b39e2a767ae4bc76bcf0cd
Cameron Lamb <cameron.lamb@torchbox.com>
2020-04-09 17:16:11 +0100

    Set context to equal None so that it is backwards compatible

--- footnotes/fields.py
+++ footnotes/fields.py
@@ -4,5 +4,5 @@ from django.db.models import UUIDField
 class CustomUUIDField(UUIDField):
     """ Returns database value as a string, so that dashes are preserved. """
 
-    def from_db_value(self, value, expression, connection):
+    def from_db_value(self, value, expression, connection, context=None):
         return str(value)
