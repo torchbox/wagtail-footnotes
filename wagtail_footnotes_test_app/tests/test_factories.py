9ca271c28fd94b5d33da8b67cb01864e72702c7c
Cameron Lamb <cameron.lamb@torchbox.com>
2021-06-16 17:30:16 +0100

    Add migrations and fix tests

--- wagtail_footnotes_test_app/tests/test_factories.py
+++ wagtail_footnotes_test_app/tests/test_factories.py
@@ -1,9 +1,11 @@
+from django.test import TestCase
+
 from ..factories import TestPage
 
 
-def test_testpage_factory():
-    TestPage()
+class TestPageFactory(TestCase):
+    def test_testpage_factory(self):
+        TestPage()
 
-
-def test_testpage_factory_with_empty_body():
-    TestPage(bady={})
+    def test_testpage_factory_with_empty_body(self):
+        TestPage(body={})
