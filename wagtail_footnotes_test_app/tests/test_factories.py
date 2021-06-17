75ed7114723159d482e0da1e8f04f7b17e68866d
Cameron Lamb <cameron.lamb@torchbox.com>
2021-06-17 15:25:09 +0100

    Move comment to correct file

--- wagtail_footnotes_test_app/tests/test_factories.py
+++ wagtail_footnotes_test_app/tests/test_factories.py
@@ -9,12 +9,3 @@ class TestPageFactory(TestCase):
 
     def test_factory_with_empty_body(self):
         factories.TestPage(body={})
-
-
-# TODO:
-#  - Create a Page with a footnote
-#  - Render a Page with a footnote
-#  - Check the rendered page for the footnote output
-#  - Check the rendered page for the footnote listing
-#  - Try saving a page where the footnote's ID is None
-#  - Try saving a page where the rich text references a footnote that no longer exists
---
+++ wagtail_footnotes_test_app/tests/test_footnotes.py
@@ -0,0 +1,7 @@
+# TODO:
+#  - Create a Page with a footnote
+#  - Render a Page with a footnote
+#  - Check the rendered page for the footnote output
+#  - Check the rendered page for the footnote listing
+#  - Try saving a page where the footnote's ID is None
+#  - Try saving a page where the rich text references a footnote that no longer exists
