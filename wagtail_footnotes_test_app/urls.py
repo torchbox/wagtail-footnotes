956e72081684091f15a751f2a12bb732573747a2
Cameron Lamb <cameron.lamb@torchbox.com>
2021-06-17 18:03:02 +0100

    Add wagtail urls to test urls

--- wagtail_footnotes_test_app/urls.py
+++ wagtail_footnotes_test_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
 
+from wagtail.core import urls as wagtail_urls
+
 from wagtail_footnotes import urls as footnotes_urls
 
 urlpatterns = [
+    path("", include(wagtail_urls)),
     path("footnotes/", include(footnotes_urls)),
 ]
