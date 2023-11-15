b46d59759458f7c10e617a63028b65d840642dd4
Katherine Domingo <katherine.domingo@torchbox.com>
2023-11-15 09:47:57 +0800

    Put Wagtail 5.2 upgrade consideration under conditional

--- wagtail_footnotes/blocks.py
+++ wagtail_footnotes/blocks.py
@@ -2,6 +2,7 @@ import re
 
 from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
+from wagtail import VERSION as WAGTAIL_VERSION
 from wagtail.blocks import RichTextBlock
 from wagtail.models import Page
 
@@ -53,7 +54,9 @@ class RichTextBlockWithFootnotes(RichTextBlock):
         return mark_safe(FIND_FOOTNOTE_TAG.sub(replace_tag, html))  # noqa: S308
 
     def render(self, value, context=None):
-        if not self.get_template(value=value, context=context):
+        kwargs = {"value": value} if WAGTAIL_VERSION >= (5, 2) else {}
+
+        if not self.get_template(context=context, **kwargs):
             return self.render_basic(value, context=context)
 
         html = super().render(value, context=context)
