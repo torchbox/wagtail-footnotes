a2ec1aeaaced49c6242b62b2f65adba5f54ae7bc
Katherine Domingo <katherine.domingo@torchbox.com>
2024-02-20 14:30:55 +0800

    Update implementation of ReadonlyUUIDInput

--- wagtail_footnotes/static/footnotes/js/read-only-uuid-controller.js
+++
@@ -1,9 +0,0 @@
-// wagtail_footnotes/static/footnotes/js/read-only-uuid-controller.js
-
-class CustomEditorController extends window.StimulusModule.Controller {
-    connect() {
-        setUUID(this.element.id);
-    }
-}
-
-window.wagtail.app.register('read-only-uuid', CustomEditorController);
